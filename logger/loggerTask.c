512d197224b5b56244880b08e03dd057f994ba5b
brent <brent@b5de89bc-dee3-0310-a7f3-e33a5ece582f>
2009-01-26 03:16:30 +0000

    added logging of GPS data, added functions to allow logger to access GPS data values

--- logger/loggerTask.c
+++ logger/loggerTask.c
@@ -9,9 +9,16 @@
 #include "string.h"
 #include "stdio.h"
 #include "sdcard.h"
+#include "gps.h"
 
 int g_loggingShouldRun;
 xSemaphoreHandle g_xLoggerStart;
+
+#define BASE_100Hz 3
+#define BASE_50Hz 6
+#define BASE_30Hz 10
+#define BASE_20Hz 15
+#define BASE_10Hz 30
 
 
 #define LOGGER_TASK_PRIORITY				( tskIDLE_PRIORITY + 4 )
@@ -87,51 +94,24 @@ void loggerTask(void *params){
 					g_loggingShouldRun = 1;
 				}
 			}
-			char buf[20];
 			
 			//signal writer to open next file
 			fileWriteString("!");
 				
 			portTickType xLastWakeTime, startTickTime;
-			const portTickType xFrequency = 3;
+			const portTickType xFrequency = BASE_10Hz;
 			startTickTime = xLastWakeTime = xTaskGetTickCount();
 			
 			
 			//run until we should not log anymore
 			while (g_loggingShouldRun){
+				
+				writeGPSPosition();
+				writeGPSVelocity();
+
 				unsigned int a0,a1,a2,a3,a4,a5,a6,a7;
 				ReadAllADC(&a0,&a1,&a2,&a3,&a4,&a5,&a6,&a7);				
-
-				modp_itoa10(a0,buf);
-				fileWriteString(buf);
-				fileWriteString(",");
-
-				modp_itoa10(a1,buf);
-				fileWriteString(buf);
-				fileWriteString(",");
-
-				modp_itoa10(a2,buf);
-				fileWriteString(buf);
-				fileWriteString(",");
-	
-				modp_itoa10(a3,buf);
-				fileWriteString(buf);
-				fileWriteString(",");
-
-				modp_itoa10(a4,buf);
-				fileWriteString(buf);
-				fileWriteString(",");
-
-				modp_itoa10(a5,buf);
-				fileWriteString(buf);
-				fileWriteString(",");
-	
-				modp_itoa10(a6,buf);
-				fileWriteString(buf);
-				fileWriteString(",");
-
-				modp_itoa10(a7,buf);
-				fileWriteString(buf);
+				writeADC(a0,a1,a2,a3,a4,a5,a6,a7);
 
 				fileWriteString("\n");			
 				
@@ -145,3 +125,80 @@ void loggerTask(void *params){
 	}
 }
 
+void writeADC(unsigned int a0,unsigned int a1,unsigned int a2,unsigned int a3,unsigned int a4,unsigned int a5,unsigned int a6,unsigned int a7){
+	
+	char buf[20];
+	modp_itoa10(a0,buf);
+	fileWriteString(buf);
+	fileWriteString(",");
+
+	modp_itoa10(a1,buf);
+	fileWriteString(buf);
+	fileWriteString(",");
+
+	modp_itoa10(a2,buf);
+	fileWriteString(buf);
+	fileWriteString(",");
+
+	modp_itoa10(a3,buf);
+	fileWriteString(buf);
+	fileWriteString(",");
+
+	modp_itoa10(a4,buf);
+	fileWriteString(buf);
+	fileWriteString(",");
+
+	modp_itoa10(a5,buf);
+	fileWriteString(buf);
+	fileWriteString(",");
+
+	modp_itoa10(a6,buf);
+	fileWriteString(buf);
+	fileWriteString(",");
+
+	modp_itoa10(a7,buf);
+	fileWriteString(buf);
+	fileWriteString(",");	
+}
+
+void writeGPSPosition(){
+	char buf[20];
+	
+	if 	(getGPSPositionUpdated()){
+		modp_itoa10(getGPSQuality(),buf);
+		fileWriteString(buf);
+		fileWriteString(",");
+
+		fileWriteString(getUTCTime());
+		fileWriteString(",");
+		
+		fileWriteString(getLatitude());
+		fileWriteString(",");
+		
+		fileWriteString(getLongitude());
+		fileWriteString(",");
+		
+		modp_itoa10(getSatellitesUsedForPosition(),buf);
+		fileWriteString(buf);
+		fileWriteString(",");
+		setGPSPositionStale();
+	}
+	else{
+		fileWriteString(",");
+		fileWriteString(",");
+		fileWriteString(",");
+		fileWriteString(",");
+		fileWriteString(",");
+	}
+}
+
+void writeGPSVelocity(){
+	if 	(getGPSVelocityUpdated()){
+		fileWriteString(getGPSVelocity());
+		fileWriteString(",");
+		setGPSVelocityStale();
+	}
+	else{
+		fileWriteString(",");	
+	}
+}
--- logger/loggerTask.h
+++ logger/loggerTask.h
@@ -8,4 +8,8 @@ void loggerTask(void *params);
 void loggerWriter(void *params);
 void fileWriteString(char *s);
 
+void writeADC(unsigned int a0,unsigned int a1,unsigned int a2,unsigned int a3,unsigned int a4,unsigned int a5,unsigned int a6,unsigned int a7);
+void writeGPSPosition();
+void writeGPSVelocity();
+
 #endif /*LOGGERTASK_H_*/
