1266c3b83f97fd152a6a560b08f6f03e84fe17e0
brent <brent@b5de89bc-dee3-0310-a7f3-e33a5ece582f>
2011-05-10 05:29:50 +0000

    removed external'd directories

--- firmware/FreeRTOS/Source/portable/MemMang/heap.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEAP_H_
-#define HEAP_H_
-#include <stdlib.h>
-
-void *pvPortMalloc( size_t xWantedSize );
-void vPortFree( void *pv );
-void * pvPortRealloc( void *pv, size_t xWantedSize );
-
-#endif
--- firmware/util/modp_atonum.h
+++
@@ -1,9 +0,0 @@
-#ifndef COM_MODP_STRINGENCODERS_ATONUM_H
-#define COM_MODP_STRINGENCODERS_ATONUM_H
-
-
-int modp_atoi(char *str);
-float modp_atof(char *str);
-double modp_atod(char *str);
-
-#endif
