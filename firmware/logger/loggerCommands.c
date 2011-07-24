836a3f947645a5fdc0c5f4192fec571494a1fab5
brent <brent@b5de89bc-dee3-0310-a7f3-e33a5ece582f>
2011-07-24 21:01:43 +0000

    re-adding commands

--- firmware/logger/loggerCommands.c
+++ firmware/logger/loggerCommands.c
@@ -4,8 +4,13 @@
  *  Created on: Jul 24, 2011
  *      Author: brent
  */
+#include <string.h>
 #include "loggerCommands.h"
 #include "usb_comm.h"
+#include "modp_numtoa.h"
+#include "modp_atonum.h"
+#include "base64.h"
+#include "luaScript.h"
 
 static struct cmd_t execLuaCmd = {"lua",
 		"Executes the specified Lua script fragment",
@@ -34,7 +39,30 @@ void ExecLuaCommand(unsigned char argc, char **argv){
 
 void WriteScriptPage(unsigned char argc, char **argv){
 
+	if (argc < 3){
+		SendString("Invalid Parameters");
+		SendCrlf();
+		ShowHelp();
+		return;
+	}
 
+	unsigned int page = modp_atoi(argv[1]);
+	char * scriptPage = argv[2];
+
+	if (scriptPage != NULL){
+		vPortEnterCritical();
+		char *decodedScript = base64decode(scriptPage, strlen(scriptPage));
+		int result = flashScriptPage(page,decodedScript);
+		vPortFree(decodedScript);
+	   	vPortExitCritical();
+		if (result == 0){
+			SendCommandOK();
+		}
+		else{
+			SendCommandError(result);
+		}
+		SendCrlf();
+	}
 }
 
 void ReadScriptPage(unsigned char argc, char **argv){
