cd582c377451492e7100b274181e7ca8e7f2bdbf
brent <brent@b5de89bc-dee3-0310-a7f3-e33a5ece582f>
2011-07-25 07:15:45 +0000

    added constants

---
+++ constants.h
@@ -0,0 +1,16 @@
+/*
+ * constants.h
+ *
+ *  Created on: Jul 23, 2011
+ *      Author: brent
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#define FIRMWARE_VERSION "1.0.0.0"
+#define WELCOME_MSG "Welcome to RaceCapture/Pro : Firmware Version " FIRMWARE_VERSION
+#define COMMAND_PROMPT "RaceCapture/Pro"
+
+
+#endif /* CONSTANTS_H_ */
