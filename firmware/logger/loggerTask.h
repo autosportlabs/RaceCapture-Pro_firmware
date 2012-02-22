3e0e2a5f3279c0ef714b0c855339f2c07e6ac54a
Autosport Labs <brent@autosportlabs.com>
2012-02-22 00:18:31 -0800

    moar changes for logger record. added macros for setting & testing NIL sample

--- firmware/logger/loggerTask.h
+++ firmware/logger/loggerTask.h
@@ -6,9 +6,11 @@
 
 #include <limits.h>
 
-#define NIL_SAMPLE INT_MIN
+#define NIL_SAMPLE_VALUE INT_MIN
+#define NIL_SAMPLE(X) X.intValue = NIL_SAMPLE_VALUE
+#define IS_NIL_SAMPLE(X) (X.intValue == NIL_SAMPLE_VALUE)
 
-struct channelSample
+struct ChannelSample
 	{
 	void * channelConfig;
 	union
@@ -18,19 +20,45 @@ struct channelSample
 	};
 };
 
-struct sampleRecord
+struct SampleRecord
 {
-	struct channelSample ADCSamples[CONFIG_ADC_CHANNELS];
-	struct channelSample PWMSamples[CONFIG_PWM_CHANNELS];
-	struct channelSample GPIOSamples[CONFIG_GPIO_CHANNELS];
-	struct channelSample TimerSamples[CONFIG_TIMER_CHANNELS];
-	struct channelSample AccelSamples[CONFIG_ACCEL_CHANNELS];
-	struct channelSample GPS_LatitueSample;
-	struct channelSample GPS_LongitudeSample;
-	struct channelSample GPS_VelocitySample;
-	struct channelSample GPS_TimeSample;
+	struct ChannelSample ADCSamples[CONFIG_ADC_CHANNELS];
+	struct ChannelSample PWMSamples[CONFIG_PWM_CHANNELS];
+	struct ChannelSample GPIOSamples[CONFIG_GPIO_CHANNELS];
+	struct ChannelSample TimerSamples[CONFIG_TIMER_CHANNELS];
+	struct ChannelSample AccelSamples[CONFIG_ACCEL_CHANNELS];
+	struct ChannelSample GPS_LatitueSample;
+	struct ChannelSample GPS_LongitudeSample;
+	struct ChannelSample GPS_VelocitySample;
+	struct ChannelSample GPS_TimeSample;
 };
 
+//Not sure if I like where this is going, playing around.
+struct SampleRecord * createNewSampleRecord(){
+	struct SampleRecord *sr = (struct SampleRecord *)malloc(sizeof(struct SampleRecord));
+
+	for (int i=0; i < CONFIG_ADC_CHANNELS;i++){
+		//set channel config pointer from respective logger config channel
+		NIL_SAMPLE(sr->ADCSamples[i]);
+	}
+	for (int i=0; i < CONFIG_PWM_CHANNELS;i++){
+		NIL_SAMPLE(sr->PWMSamples[i]);
+	}
+	for (int i=0; i < CONFIG_GPIO_CHANNELS;i++){
+		NIL_SAMPLE(sr->GPIOSamples[i]);
+	}
+	for (int i=0; i < CONFIG_TIMER_CHANNELS;i++){
+		NIL_SAMPLE(sr->TimerSamples[i]);
+	}
+	for (int i=0; i < CONFIG_ACCEL_CHANNELS;i++){
+		NIL_SAMPLE(sr->AccelSamples[i]);
+	}
+	NIL_SAMPLE(sr->GPS_LatitueSample);
+	NIL_SAMPLE(sr->GPS_LongitudeSample);
+	NIL_SAMPLE(sr->GPS_VelocitySample);
+	NIL_SAMPLE(sr->GPS_TimeSample);
+	return sr;
+}
 
 void createLoggerTask();
 void loggerTask(void *params);
