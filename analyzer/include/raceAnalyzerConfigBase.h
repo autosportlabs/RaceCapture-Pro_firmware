c88112fbc1134e811ae75b9145c18b29a30d1f3f
U-aslwindev\brent <brent@aslwindev.(none)>
2012-08-19 17:03:33 -0700

    update for release 0.0.2

--- analyzer/include/raceAnalyzerConfigBase.h
+++ analyzer/include/raceAnalyzerConfigBase.h
@@ -1,8 +1,8 @@
 #ifndef RACEANALYZERCONFIGBASE_H_
 #define RACEANALYZERCONFIGBASE_H_
 
-#define		RACE_ANALYZER_VERSION			"0.0.1"
-#define 	RACE_ANALYZER_APP_NAME 			"Autosport_Labs_RaceAnalyzer_0.0.1"
+#define		RACE_ANALYZER_VERSION			"0.0.2"
+#define 	RACE_ANALYZER_APP_NAME 			"Autosport_Labs_RaceAnalyzer_0.0.2"
 
 
 class InvalidStateException{
