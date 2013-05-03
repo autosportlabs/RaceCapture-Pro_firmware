505361ff501898053d90ef99ddd60b49504e6297
Brent Picasso <brent@autosportlabs.com>
2013-05-02 22:49:25 -0700

    reorganized

---
+++ test/loggerData_test.cpp
@@ -0,0 +1,24 @@
+
+#include "loggerData_test.h"
+#include "loggerData.h"
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( LoggerDataTest );
+
+
+void
+LoggerDataTest::setUp()
+{
+}
+
+
+void
+LoggerDataTest::tearDown()
+{
+}
+
+
+void
+LoggerDataTest::testPopulateAnalogChannels()
+{
+
+}
