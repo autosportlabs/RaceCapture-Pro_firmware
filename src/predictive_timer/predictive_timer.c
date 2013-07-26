f523cb0b77f7d02a67152954bed7451b3b8d134a
Brent Picasso <brent@autosportlabs.com>
2013-07-25 19:51:43 -0700

    bugfixing, more testing on auto-compacting predictive lap timer buffer

--- src/predictive_timer/predictive_timer.c
+++ src/predictive_timer/predictive_timer.c
@@ -25,12 +25,13 @@ void end_lap(){
 static void compact_lap_buffer(LapBuffer *buffer){
 	size_t sampleCount = buffer->sampleCount;
 	size_t newCount = buffer->sampleCount / 2;
-	for (size_t i = 0; i < sampleCount; i+=2){
+	size_t targetIndex = 0;
+	for (size_t i = 0; i < sampleCount; i+=2,targetIndex++){
 		LocationSample *s1 = &buffer->samples[i];
-		LocationSample *s2 = &buffer->samples[i+2];
-
-		s1->distance = s1->distance + s2->distance;
-		s1->speed = AVERAGE_TWO(s1->speed, s2->speed);
+		LocationSample *s2 = &buffer->samples[i+1];
+		LocationSample *target = &buffer->samples[targetIndex];
+		target->distance = s1->distance + s2->distance;
+		target->speed = AVERAGE_TWO(s1->speed, s2->speed);
 	}
 	buffer->sampleInterval++;
 	buffer->sampleCount = newCount;
@@ -46,12 +47,14 @@ void add_sample(float speed, float distance){
 		if (currentLapBuffer.sampleCount >= MAX_LOCATION_SAMPLES){
 			compact_lap_buffer(&currentLapBuffer);
 		}
-		LocationSample *sample = &currentLapBuffer.samples[currentLapBuffer.sampleCount++];
-		sample->distance = currentLapBuffer.currentDistAccumulator;
-		sample->speed = currentLapBuffer.currentSpeedAccumulator / (float)currentLapBuffer.sampleInterval;
-		currentLapBuffer.currentDistAccumulator = 0;
-		currentLapBuffer.currentSpeedAccumulator = 0;
-		currentLapBuffer.currentInterval = 0;
+		else{
+			LocationSample *sample = &currentLapBuffer.samples[currentLapBuffer.sampleCount++];
+			sample->distance = currentLapBuffer.currentDistAccumulator;
+			sample->speed = currentLapBuffer.currentSpeedAccumulator / (float)currentLapBuffer.sampleInterval;
+			currentLapBuffer.currentDistAccumulator = 0;
+			currentLapBuffer.currentSpeedAccumulator = 0;
+			currentLapBuffer.currentInterval = 0;
+		}
 	}
 }
 
--- test/predictiveTime_test.cpp
+++ test/predictiveTime_test.cpp
@@ -26,4 +26,10 @@ void PredictiveTimeTest::testAddSample()
 
 	CPPUNIT_ASSERT_EQUAL((size_t)10, currentBuffer->sampleCount);
 	CPPUNIT_ASSERT_EQUAL((size_t)2, currentBuffer->sampleInterval);
+
+	size_t totalDistance = 0;
+	for (size_t i = 0; i < 10; i++){
+		totalDistance += currentBuffer->samples[i].distance;
+	}
+	CPPUNIT_ASSERT_EQUAL((size_t)20, totalDistance);
 }
