72f37457cfc1077b8896ad73e7ec3a9465a6a97d
Brent Picasso <brent@autosportlabs.com>
2013-10-30 20:12:44 -0700

    added logging of int values

--- include/race_capture/printk.h
+++ include/race_capture/printk.h
@@ -20,14 +20,20 @@ enum log_level {
 };
 
 #define pr_info(arg1) printk(INFO, arg1)
-#define pr_debug(arg1) printk(DEBUG, arg1)
-#define pr_warning(arg1) printk(WARNING, arg1)
-#define pr_error(arg1) printk(ERR, arg1)
+#define pr_info_int(arg1) printk_int(INFO, arg1);
 
+#define pr_debug(arg1) printk(DEBUG, arg1);
+#define pr_debug_int(arg1) printk_int(DEBUG, arg1);
 
+#define pr_warning(arg1) printk(WARNING, arg1);
+#define pr_warning_int(arg1) printk_int(WARNING, arg1);
+
+#define pr_error(arg1) printk(ERR, arg1);
+#define pr_error_int(arg1) printk_int(ERR, arg1);
 
 size_t read_log_to_serial(Serial *s);
 int printk(enum log_level level, const char *msg);
+int printk_int(enum log_level level, int value);
 enum log_level set_log_level(enum log_level level);
 
 #endif /* __PRINTK_H__ */
