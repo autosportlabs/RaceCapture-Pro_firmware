0510ea83bfffbed23befd57c0b27d93de650a403
Brent Picasso <brent@autosportlabs.com>
2014-01-21 11:26:35 -0800

    added ability for lua to log messages

--- SAM7s_base/lua/luaBaseBinding.c
+++ SAM7s_base/lua/luaBaseBinding.c
@@ -15,14 +15,19 @@
 #include "command.h"
 #include "taskUtil.h"
 #include "luaTask.h"
+#include "printk.h"
+
 
 void registerBaseLuaFunctions(lua_State *L){
-	//Utility
 	lua_registerlight(L,"print",Lua_Print);
 	lua_registerlight(L,"println", Lua_Println);
 	lua_registerlight(L,"getStackSize", Lua_GetStackSize);
 	lua_registerlight(L,"setTickRate", Lua_SetTickRate);
 	lua_registerlight(L,"getTickRate", Lua_GetTickRate);
+	lua_registerlight(L,"log", Lua_WriteLog);
+	lua_registerlight(L,"logln", Lua_WriteLogLn);
+	lua_registerlight(L,"setLogLevel", Lua_SetLogLevel);
+	lua_registerlight(L,"getLogLevel", Lua_GetLogLevel);
 }
 
 int Lua_Println(lua_State *L){
@@ -60,3 +65,34 @@ int Lua_GetTickRate(lua_State *L){
 	return 1;
 }
 
+int Lua_WriteLogLn(lua_State *L){
+	if (lua_gettop(L) >= 2){
+		int level = lua_tointeger(L, 1);
+		const char *msg = lua_tostring(L, 2);
+		printk(level, msg);
+		printk(level, "\r\n");
+	}
+	return 0;
+}
+
+int Lua_WriteLog(lua_State *L){
+	if (lua_gettop(L) >= 2){
+		int level = lua_tointeger(L, 1);
+		const char *msg = lua_tostring(L, 2);
+		printk(level, msg);
+	}
+	return 0;
+}
+
+int Lua_SetLogLevel(lua_State *L){
+	if (lua_gettop(L) >= 1){
+		int level = lua_tointeger(L, 1);
+		set_log_level(level);
+	}
+	return 0;
+}
+
+int Lua_GetLogLevel(lua_State *L){
+	lua_pushinteger(L, get_log_level());
+	return 1;
+}
--- SAM7s_base/lua/luaBaseBinding.h
+++ SAM7s_base/lua/luaBaseBinding.h
@@ -19,4 +19,9 @@ int Lua_Println(lua_State *L);
 int Lua_GetStackSize(lua_State *L);
 int Lua_SetTickRate(lua_State *L);
 int Lua_GetTickRate(lua_State *L);
+int Lua_WriteLog(lua_State *L);
+int Lua_WriteLogLn(lua_State *L);
+int Lua_SetLogLevel(lua_State *L);
+int Lua_GetLogLevel(lua_State *L);
+
 #endif /* LUABASEBINDINGS_H_ */
