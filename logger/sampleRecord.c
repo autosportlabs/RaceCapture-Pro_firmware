2ad608300ccd20bf35983e74c1f1849fa546fa53
Brent Picasso <brent@autosportlabs.com>
2014-02-01 20:31:16 -0800

    WIP for dynamic channel logging

--- logger/sampleRecord.c
+++ logger/sampleRecord.c
@@ -8,6 +8,51 @@
 #include "loggerConfig.h"
 #include "mem_mang.h"
 
+ChannelSample ** create_channel_sample_buffer(LoggerConfig *loggerConfig){
+	size_t channelCount = get_enabled_channel_count(loggerConfig);
+	ChannelSample ** samples = portMalloc(sizeof(ChannelSample[channelCount]));
+	return samples;
+}
+
+void init_channel_sample_buffer(ChannelSample ** samples, size_t count){
+	ChannelSample *sample = samples[0];
+
+	//todo WIP!!
+
+	for (int i=0; i < CONFIG_ACCEL_CHANNELS; i++){
+		if (loggerConfig->AccelConfigs[i].cfg.sampleRate != SAMPLE_DISABLED) channels++;
+	}
+
+	for (int i=0; i < CONFIG_ADC_CHANNELS; i++){
+		if (loggerConfig->ADCConfigs[i].cfg.sampleRate != SAMPLE_DISABLED) channels++;
+	}
+
+	for (int i=0; i < CONFIG_TIMER_CHANNELS; i++){
+		if (loggerConfig->TimerConfigs[i].cfg.sampleRate != SAMPLE_DISABLED) channels++;
+	}
+
+	for (int i=0; i < CONFIG_GPIO_CHANNELS; i++){
+		if (loggerConfig->GPIOConfigs[i].cfg.cfg.sampleRate != SAMPLE_DISABLED) channels++;
+	}
+
+	for (int i=0; i < CONFIG_PWM_CHANNELS; i++){
+		if (loggerConfig->PWMConfigs[i].cfg.sampleRate != SAMPLE_DISABLED) channels++;
+	}
+
+	if (loggerConfig->GPSConfigs.latitudeCfg.sampleRate != SAMPLE_DISABLED) channels++;
+	if (loggerConfig->GPSConfigs.longitudeCfg.sampleRate != SAMPLE_DISABLED) channels++;
+	if (loggerConfig->GPSConfigs.speedCfg.sampleRate != SAMPLE_DISABLED) channels++;
+	if (loggerConfig->GPSConfigs.timeCfg.sampleRate != SAMPLE_DISABLED) channels++;
+	if (loggerConfig->GPSConfigs.satellitesCfg.sampleRate != SAMPLE_DISABLED) channels++;
+
+	if (loggerConfig->TrackConfigs.lapCountCfg.sampleRate != SAMPLE_DISABLED) channels++;
+	if (loggerConfig->TrackConfigs.lapTimeCfg.sampleRate != SAMPLE_DISABLED) channels++;
+	if (loggerConfig->TrackConfigs.splitTimeCfg.sampleRate != SAMPLE_DISABLED) channels++;
+	if (loggerConfig->TrackConfigs.distanceCfg.sampleRate != SAMPLE_DISABLED) channels++;
+	if (loggerConfig->TrackConfigs.predTimeCfg.sampleRate != SAMPLE_DISABLED) channels++;
+
+
+}
 void initSampleRecord(LoggerConfig *loggerConfig,SampleRecord *sr){
 
 	for (int i=0; i < CONFIG_ADC_CHANNELS; i++){
