5a21ff2f42e8e6ece2beeb72efce47573550b09b
Brent Picasso <brent@autosportlabs.com>
2014-02-22 14:06:41 -0800

    adust memory page size if passed in segment length is less than the min page size

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -3,5 +3,6 @@
 
 
 int memory_flash_region(const void *address, const void *data, unsigned int length){
+	if (length < MEMORY_PAGE_SIZE) length = length + (MEMORY_PAGE_SIZE - length);
 	return memory_device_flash_region(address, data, length);
 }
