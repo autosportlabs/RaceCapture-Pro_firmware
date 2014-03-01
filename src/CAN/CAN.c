2589f19c50f42113b638887e10ce789c1ef22b3c
Brent Picasso <brent@autosportlabs.com>
2014-02-28 21:57:31 -0800

    only call CAN device functions if CAN is initialized, otherwise return false

--- src/CAN/CAN.c
+++ src/CAN/CAN.c
@@ -2,15 +2,17 @@
 #include "printk.h"
 #include "CAN_device.h"
 
+static int CAN_is_init = 0;
 
 int CAN_init(int baud){
-	return CAN_device_init(baud);
+	CAN_is_init = CAN_device_init(baud);
+	return CAN_is_init;
 }
 
 int CAN_tx_msg(CAN_msg *msg, unsigned int timeoutMs){
-	return CAN_device_tx_msg(msg, timeoutMs);
+	return CAN_is_init && CAN_device_tx_msg(msg, timeoutMs);
 }
 
 int CAN_rx_msg(CAN_msg *msg, unsigned int timeoutMs){
-	return CAN_device_rx_msg(msg, timeoutMs);
+	return CAN_is_init && CAN_device_rx_msg(msg, timeoutMs);
 }
