9aeb19709e73b67efd999874f258433125785623
Brent Picasso <brent@autosportlabs.com>
2014-03-06 11:55:59 -0800

    added some debug logging

--- src/GPIO/gpioTasks.c
+++ src/GPIO/gpioTasks.c
@@ -8,8 +8,10 @@
 #include "loggerHardware.h"
 #include "loggerTaskEx.h"
 #include "GPIO_device.h"
+#include "taskUtil.h"
+#include "printk.h"
 
-#define DEBOUNCE_DELAY_PERIOD		( ( portTickType )30 / portTICK_RATE_MS  )
+#define DEBOUNCE_DELAY_PERIOD		30
 #define GPIO_TASK_STACK_SIZE		50
 
 xSemaphoreHandle xOnPushbutton;
@@ -17,25 +19,25 @@ xSemaphoreHandle xOnPushbutton;
 void startGPIOTasks(int priority){
 	
 	vSemaphoreCreateBinary( xOnPushbutton );
-	
-
+	xSemaphoreTake(xOnPushbutton, 0);
 	xTaskCreate( onPushbuttonTask, 	( signed portCHAR * ) "PushbuttonTask", 	GPIO_TASK_STACK_SIZE, 	NULL, 	priority, 	NULL );
-
 }
 
 
 void onPushbuttonTask(void *pvParameters){
 	
-	portTickType xDelayPeriod = DEBOUNCE_DELAY_PERIOD;
-	
 	while(1){
 		if ( xSemaphoreTake(xOnPushbutton, portMAX_DELAY) == pdTRUE){
-			vTaskDelay( xDelayPeriod );
+			delayMs(DEBOUNCE_DELAY_PERIOD);
+
 			if (GPIO_device_is_button_pressed()){
+				pr_info("button pressed\r\n");
 				if (isLogging()){
+					pr_info("logging stop\r\n");
 					stopLogging();
 				}
 				else{
+					pr_info("logging start\r\n");
 					startLogging();
 				}
 			}
