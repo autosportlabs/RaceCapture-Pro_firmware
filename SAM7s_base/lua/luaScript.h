ba731563cbc8a88b947385176a77de3c9d494a8b
Brent Picasso <brent@autosportlabs.com>
2014-03-29 08:49:08 -0700

    restored default script

--- SAM7s_base/lua/luaScript.h
+++ SAM7s_base/lua/luaScript.h
@@ -12,17 +12,8 @@ unsigned int getPageSize();
 unsigned int getScriptPages();
 
 
-//#define DEFAULT_SCRIPT "function onTick() end"
-
-#define DEFAULT_SCRIPT \
-"setTickRate(15) \n \
-function onTick()  \n \
-yaw=getAnalog(3) \n \
-if yaw < 0 then yaw = -yaw end \n \
-if yaw > 10 then println(\"foo\") end \n \
-if yaw > 10 then setGpio(0,1) else setGpio(0,0) end \n \
--if yaw > 20 then setGpio(1,1) else setGpio(1,0) end \n \
-if yaw > 30 then setGpio(2,1) else setGpio(2,0) end \n \
-end \n "
+#define DEFAULT_SCRIPT "function onTick() end"
 	
+
+
 #endif /*LUASCRIPT_H_*/
