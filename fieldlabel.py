e782ab8281401f7aaf73be0a90b0d21f49f52ddd
Brent Picasso <brent@autosportlabs.com>
2014-04-19 11:32:26 -0700

    added fieldlabel that can halign properly

---
+++ fieldlabel.py
@@ -0,0 +1,14 @@
+
+import kivy
+kivy.require('1.0.8')
+
+from kivy.uix.label import Label
+
+class FieldLabel(Label):
+    def __init__(self, **kwargs):
+        super(FieldLabel, self).__init__(**kwargs)
+        self.bind(width=self.width_changed)
+        self.spacing = (20,3)
+        
+    def width_changed(self, instance, size):
+        self.text_size = (size, instance.height)
