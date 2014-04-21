c913f1611feba8cc9f388b62b64c8473a58ca752
Brent Picasso <brent@autosportlabs.com>
2014-04-21 08:08:39 -0700

    updated track configuration view to match firmware track configuration

--- trackconfigview.py
+++ trackconfigview.py
@@ -3,19 +3,30 @@ kivy.require('1.8.0')
 
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.app import Builder
+from utils import *
 
 Builder.load_file('trackconfigview.kv')
 
-class TargetConfigView(GridLayout):
+class SectorPointView(BoxLayout):
     def __init__(self, **kwargs):
-        super(TargetConfigView, self).__init__(**kwargs)
-
+        super(SectorPointView, self).__init__(**kwargs)
+    
 class TrackConfigView(BoxLayout):
     def __init__(self, **kwargs):
         super(TrackConfigView, self).__init__(**kwargs)
-#        Builder.load_file('analogchannelsview.kv')
         self.register_event_type('on_config_updated')
+        
+        sectors = kvFind(self, 'rcid', 'sectorsGrid')
+        
+        for i in range(20):
+            sectorView = SectorPointView()
+            sectors.add_widget(sectorView)  
+            
+        sectors.height = 35 * 20
+        sectors.size_hint = (1.0, None)
+        
 
     def on_config_updated(self, rcpCfg):
         pass
