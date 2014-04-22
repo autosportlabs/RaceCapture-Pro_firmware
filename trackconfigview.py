4c3d1e45e5db0152f94508387f15f67e1320ca55
Brent Picasso <brent@autosportlabs.com>
2014-04-21 17:16:58 -0700

    refined UI for trackconfig view; adjusted font sizes

--- trackconfigview.py
+++ trackconfigview.py
@@ -12,21 +12,38 @@ Builder.load_file('trackconfigview.kv')
 class SectorPointView(BoxLayout):
     def __init__(self, **kwargs):
         super(SectorPointView, self).__init__(**kwargs)
+        label = kvFind(self, 'rcid', 'title')
+        label.text = kwargs.get('title', 'Sector')
+
     
 class TrackConfigView(BoxLayout):
     def __init__(self, **kwargs):
         super(TrackConfigView, self).__init__(**kwargs)
+        self.separateStartFinish = False        
         self.register_event_type('on_config_updated')
         
         sectors = kvFind(self, 'rcid', 'sectorsGrid')
-        
-        for i in range(20):
-            sectorView = SectorPointView()
-            sectors.add_widget(sectorView)  
+        self.addSectorViews(sectors)
             
         sectors.height = 35 * 20
         sectors.size_hint = (1.0, None)
-        
+    
+    def addSectorViews(self, sectors):
+        startPoint = None
+        endPoint = None
+        if not self.separateStartFinish:
+            startPoint = SectorPointView(title='Start / Finish')
+        else:
+            startPoint = SectorPointView(title='Start Line')
+            endPoint = SectorPointView(title='Finish Line')
+            
+        if startPoint: 
+            sectors.add_widget(startPoint)
+        for i in range(1,20):
+            sectorView = SectorPointView(title = 'Sector ' + str(i))
+            sectors.add_widget(sectorView)
+        if endPoint:
+            sectors.add_widget(endPoint)
 
     def on_config_updated(self, rcpCfg):
         pass
