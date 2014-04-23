610a17f6f1ca5e08da2fa9a6050ce7b88970f4aa
Brent Picasso <brent@autosportlabs.com>
2014-04-22 19:30:05 -0700

    moar binding custom properties, WIP

--- settingsview.py
+++ settingsview.py
@@ -8,26 +8,27 @@ from fieldlabel import FieldLabel
 from helplabel import HelpLabel
 from kivy.app import Builder
 from utils import *
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 
 Builder.load_file('settingsview.kv')
 
 class SettingsView(BoxLayout):
     help_text = StringProperty('')
     label_text = StringProperty('')
+    edit_widget = ObjectProperty(None)
     
     def __init__(self, **kwargs):
         super(SettingsView, self).__init__(**kwargs)
         self.bind(help_text = self.on_help_text)
         self.bind(label_text = self.on_label_text)
         
-        
     def on_help_text(self, instance, value):
-        pass
+        help = kvFind(self, 'rcid', 'helpLabel')
+        help.text = value
 
     def on_label_text(self, instance, value):
-        pass
-        
+        label = kvFind(self, 'rcid', 'fieldLabel')
+        label.text = value
         
 class SettingsViewSwitch(SettingsView):
     def __init__(self, **kwargs):
