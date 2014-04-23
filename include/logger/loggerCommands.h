74a8f208d70f73f3a8f9fb948161e92afb6896de
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2014-04-22 22:04:44 -0700

    Add support to print NMEA output to the terminal
    This commit gives devs basic access to the NMEA strings
    as provided by the GPS Mouse.  This will allow us to better
    view and debug data as returned to us by the mouse itself.

--- include/logger/loggerCommands.h
+++ include/logger/loggerCommands.h
@@ -16,7 +16,8 @@
 \
 {"startTerminal", "Starts a debugging terminal session on the specified port.","<port> <baud>", StartTerminal },\
 {"viewLog", "Prints out logging messages to the terminal as they happen", "", ViewLog },\
-{"setLogLevel", "Sets the log level", "<level>", SetLogLevel }
+{"setLogLevel", "Sets the log level", "<level>", SetLogLevel },\
+{"logGpsData", "Enables logging of raw GPS data from the GPS Mouse", "<1|0>", LogGpsData }
 
 void ResetConfig(Serial *serial, unsigned int argc, char **argv);
 void TestSD(Serial *serial, unsigned int argc, char **argv);
@@ -24,5 +25,7 @@ void TestSD(Serial *serial, unsigned int argc, char **argv);
 void StartTerminal(Serial *serial, unsigned int argc, char **argv);
 void ViewLog(Serial *serial, unsigned int argc, char **argv);
 void SetLogLevel(Serial *serial, unsigned int argc, char **argv);
+void LogGpsData(Serial *serial, unsigned int argc, char **argv);
+
 
 #endif /* LOGGERCOMMANDS_H_ */
--- src/gps/gpsTask.c
+++ src/gps/gpsTask.c
@@ -6,21 +6,38 @@
  */
 #include "gpsTask.h"
 #include "gps.h"
-#include "usart.h"
 #include "FreeRTOS.h"
+#include "printk.h"
 #include "task.h"
+#include "usart.h"
 
 #define GPS_DATA_LINE_BUFFER_LEN 	200
 #define GPS_TASK_STACK_SIZE			130
 
 static char g_GPSdataLine[GPS_DATA_LINE_BUFFER_LEN];
+static bool g_enableGpsDataLogging = false;
 
-void GPSTask( void *pvParameters ){
-	for( ;; )
-	{
-		int len = usart1_readLine(g_GPSdataLine, GPS_DATA_LINE_BUFFER_LEN);
-		processGPSData(g_GPSdataLine, len);
-	}
+void setGpsDataLogging(bool enable) {
+   g_enableGpsDataLogging = enable;
+}
+
+/**
+ * Prints the output to the serial port if it is set.
+ * @param buf The buffer containing the read in line.
+ * @param len The length of the string in the buffer.
+ */
+static void logGpsInput(const char *buf, int len) {
+   if (!g_enableGpsDataLogging) return;
+   pr_info(buf);
+}
+
+void GPSTask(void *pvParameters) {
+   for (;;) {
+      int len = usart1_readLine(g_GPSdataLine, GPS_DATA_LINE_BUFFER_LEN - 1);
+      g_GPSdataLine[len] = '\0';
+      logGpsInput(g_GPSdataLine, len);
+      processGPSData(g_GPSdataLine, len);
+   }
 }
 
 void startGPSTask(int priority){
