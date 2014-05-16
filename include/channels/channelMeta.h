f9793a1c3c12ce9d4db8d6db014a3701beaaebca
Brent Picasso <brent@autosportlabs.com>
2014-05-15 19:11:18 -0700

    added add/append channel logic for channelMeta

--- include/channels/channelMeta.h
+++ include/channels/channelMeta.h
@@ -2,9 +2,11 @@
 #define CHANNELMETA_H_
 #include "stddef.h"
 
-#define CHANNEL_COUNT			100
-#define DEFAULT_LABEL_LENGTH	11
-#define DEFAULT_UNITS_LENGTH	11
+#define CHANNEL_COUNT				100
+#define DEFAULT_LABEL_LENGTH		11
+#define DEFAULT_UNITS_LENGTH		11
+
+#define CHANNEL_SYSTEM_CHANNEL_FLAG 0
 
 typedef struct _ChannelName{
 	char label[DEFAULT_LABEL_LENGTH];
@@ -23,7 +25,9 @@ const Channel * get_channel(size_t id);
 const Channels * get_channels();
 size_t filter_channel_id(size_t id);
 size_t find_channel_id(const char * name);
-
+int is_system_channel(Channel *channel);
+int flash_channels(const Channels *source, size_t rawSize);
+int add_channel(const Channel *channel, int mode, size_t index);
 #define DEFAULT_GPS_POSITION_PRECISION 		6
 #define DEFAULT_GPS_RADIUS_PRECISION 		5
 #define DEFAULT_VOLTAGE_SCALING_PRECISION	2
--- src/channels/channelMeta.c
+++ src/channels/channelMeta.c
@@ -2,6 +2,7 @@
 #include "mod_string.h"
 #include "printk.h"
 #include "memory.h"
+#include "mem_mang.h"
 
 #ifndef RCP_TESTING
 #include "memory.h"
@@ -10,7 +11,18 @@ static const Channels g_channels __attribute__ ((aligned (FLASH_MEMORY_PAGE_SIZE
 static const Channels g_channels = DEFAULT_CHANNEL_META;
 #endif
 
+#define CHANNEL_ADD_RESULT_OK  		 	1
+#define CHANNEL_ADD_RESULT_FAIL  		0
+
+#define CHANNEL_ADD_MODE_IN_PROGRESS	0
+#define CHANNEL_ADD_MODE_COMPLETE 		2
+
 static const Channels g_defaultChannelMeta = DEFAULT_CHANNEL_META;
+static Channels * g_channelsMetaBuffer = NULL;
+
+int is_system_channel(Channel *channel){
+	return channel != NULL && (channel->flags & CHANNEL_SYSTEM_CHANNEL_FLAG);
+}
 
 const Channels * get_channels(){
 	return &g_channels;
@@ -35,7 +47,54 @@ size_t find_channel_id(const char * name){
 
 int flash_default_channels(void){
 	pr_info("flashing default channels...");
-	int result = memory_flash_region(&g_channels, &g_defaultChannelMeta, sizeof (Channels));
+	int result = flash_channels(&g_defaultChannelMeta, sizeof (g_defaultChannelMeta));
+	return result;
+}
+
+int flash_channels(const Channels *source, size_t rawSize){
+	int result = memory_flash_region(source, &g_defaultChannelMeta, rawSize);
 	if (result == 0) pr_info("success\r\n"); else pr_info("failed\r\n");
+	return result;
+}
+
+int add_channel(const Channel *channel, int mode,  size_t index){
+	int result = CHANNEL_ADD_RESULT_OK;
+
+	if (index < CHANNEL_COUNT){
+		if (mode == CHANNEL_ADD_MODE_IN_PROGRESS || mode == CHANNEL_ADD_MODE_COMPLETE){
+			if (g_channelsMetaBuffer == NULL){
+				pr_info("allocating new channels buffer\r\n");
+				g_channelsMetaBuffer = (Channels *)portMalloc(sizeof(Channels));
+				memcpy(g_channelsMetaBuffer, &g_channels, sizeof(Channels));
+			}
+
+			if (g_channelsMetaBuffer != NULL){
+				Channel *channelToAdd = g_channelsMetaBuffer->channels + index;
+				memcpy(channelToAdd, channel, sizeof(Channel));
+				g_channelsMetaBuffer->count = index + 1;
+
+				if (mode == CHANNEL_ADD_MODE_COMPLETE){
+					pr_info("completed updating channels, flashing: ");
+					if (flash_channels(g_channelsMetaBuffer, sizeof(Channels)) == 0){
+						pr_info("success\r\n");
+					}
+					else{
+						pr_error("error\r\n");
+						result = CHANNEL_ADD_RESULT_FAIL;
+					}
+					portFree(g_channelsMetaBuffer);
+					g_channelsMetaBuffer = NULL;
+				}
+			}
+			else{
+				pr_error("could not allocate buffer for tracks\r\n");
+				result = CHANNEL_ADD_RESULT_FAIL;
+			}
+		}
+	}
+	else{
+		pr_error("invalid channel index\r\n");
+		result = CHANNEL_ADD_RESULT_FAIL;
+	}
 	return result;
 }
