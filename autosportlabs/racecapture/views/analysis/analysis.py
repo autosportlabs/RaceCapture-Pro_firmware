52fda8b4eeb64e35eedaba78e994939f4175fdc8
Brent Picasso <brent@autosportlabs.com>
2014-05-27 23:33:03 -0700

    added placeholder views for analysis, dashboard, tracks

---
+++ autosportlabs/racecapture/views/analysis/analysis.py
@@ -0,0 +1,10 @@
+import kivy
+kivy.require('1.8.0')
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import Builder
+
+Builder.load_file('autosportlabs/racecapture/views/analysis/analysis.kv')
+
+class AnalysisView(BoxLayout):
+    pass
+    
---
+++ autosportlabs/racecapture/views/dashboard/dashboardview.py
@@ -0,0 +1,10 @@
+import kivy
+kivy.require('1.8.0')
+from kivy.uix.boxlayout import BoxLayout
+from kivy.app import Builder
+
+Builder.load_file('autosportlabs/racecapture/views/dashboard/dashboard.kv')
+
+class DashboardView(BoxLayout):
+    pass
+    
