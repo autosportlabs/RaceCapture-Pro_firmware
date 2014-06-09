30ce59057ce23b921036962071acabccf2dce2c6
Brent Picasso <brent@autosportlabs.com>
2014-06-09 12:59:48 -0700

    fixed binding for on_channels_updated for tracksview

--- autosportlabs/racecapture/views/tracks/tracksview.py
+++ autosportlabs/racecapture/views/tracks/tracksview.py
@@ -6,5 +6,9 @@ from kivy.app import Builder
 Builder.load_file('autosportlabs/racecapture/views/tracks/tracksview.kv')
 
 class TracksView(BoxLayout):
-    pass
-    
+    def __init__(self, **kwargs):
+        super(TracksView, self).__init__(**kwargs)
+        self.register_event_type('on_channels_updated')
+
+    def on_channels_updated(self, channels):
+        pass
