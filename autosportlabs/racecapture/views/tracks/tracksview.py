55a7fb0054a466a0fc9513beeb4f9c86ec77b347
Brent Picasso <brent@autosportlabs.com>
2014-06-10 08:46:04 -0700

    WIP for trackmap viewer

--- autosportlabs/racecapture/views/tracks/tracksview.py
+++ autosportlabs/racecapture/views/tracks/tracksview.py
@@ -2,13 +2,20 @@ import kivy
 kivy.require('1.8.0')
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import Builder
+from autosportlabs.uix.track.trackmap import TrackMap
+from autosportlabs.uix.track.racetrackview import RaceTrackView
+from utils import *
+from autosportlabs.racecapture.geo.geopoint import GeoPoint
 
 Builder.load_file('autosportlabs/racecapture/views/tracks/tracksview.kv')
 
 class TracksView(BoxLayout):
+    trackmap = None
+    
     def __init__(self, **kwargs):
         super(TracksView, self).__init__(**kwargs)
         self.register_event_type('on_channels_updated')
-
+        
     def on_channels_updated(self, channels):
         pass
+    
--- autosportlabs/uix/track/trackmap.py
+++ autosportlabs/uix/track/trackmap.py
@@ -2,9 +2,16 @@ import kivy
 import math
 kivy.require('1.8.0')
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+from kivy.uix.scatter import Scatter
 from kivy.app import Builder
+from kivy.metrics import dp
+from kivy.graphics import Color, Line
+from autosportlabs.racecapture.geo.geopoint import GeoPoint
+from utils import *
 
+Builder.load_file('autosportlabs/uix/track/trackmap.kv')
 
 class Point:
     x = 0.0
@@ -12,11 +19,29 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
+    
 class TrackMap(Widget):
-    offsetPoint = Point()
+    MIN_PADDING = dp(1)
+    offsetPoint = Point(0,0)
+    globalRatio = 0
+    heightPadding = 0
+    widthPadding = 0
+    
     def __init__(self, **kwargs):
         super(TrackMap, self).__init__(**kwargs)
+        
+    def setTrackPoints(self, geoPoints):
+        points = self.genMapPoints(geoPoints)
+        linePoints = []
+        for point in points:
+            scaledPoint = self.scalePoint(point, self.height)
+            linePoints.append(scaledPoint.x)
+            linePoints.append(scaledPoint.y)
+        
+        with self.canvas:
+            Color(1., 0, 0)
+            Line(points=linePoints, width=dp(2), closed=True)
+        
         
     def projectPoint(self, geoPoint):
         latitude = geoPoint.latitude * float(math.pi / 180.0)
@@ -24,5 +49,58 @@ class TrackMap(Widget):
         point = Point(longitude, float(math.log(math.tan((math.pi / 4.0) + 0.5 * latitude))))
         return point;
 
+    def scalePoint(self, point, height):
+        adjustedX = int((self.widthPadding + (point.x * self.globalRatio)))
+        #need to invert the Y since 0,0 starts at top left
+        adjustedY = int((self.heightPadding + (point.y * self.globalRatio)))
+        return Point(adjustedX, adjustedY)
 
-    
+    def genMapPoints(self, geoPoints):
+        points = []
+        
+        # min and max coordinates, used in the computation below
+        minXY = Point(-1, -1)
+        maxXY = Point(-1, -1)
+        
+        for geoPoint in geoPoints:
+            point = self.projectPoint(geoPoint)
+            minXY.x = point.x if minXY.x == -1 else min(minXY.x, point.x)
+            minXY.y = point.y if minXY.y == -1 else min(minXY.y, point.y)
+            points.append(point);
+        
+        # now, we need to keep track the max X and Y values
+        for point in points:
+            point.x = point.x - minXY.x
+            point.y = point.y - minXY.y
+            maxXY.x = point.x if maxXY.x == -1 else max(maxXY.x, point.x)
+            maxXY.y = point.y if maxXY.y == -1 else max(maxXY.y, point.y);
+        
+        
+        paddingBothSides = self.MIN_PADDING * 2
+        
+        width = float(self.width)
+        height = float(self.height)
+
+        print(str(width) + ' ' + str(height))        
+        # the actual drawing space for the map on the image
+        mapWidth = width - paddingBothSides;
+        mapHeight = height - paddingBothSides;
+
+        print(str(mapWidth) + ' ' + str(mapHeight))
+        #determine the width and height ratio because we need to magnify the map to fit into the given image dimension
+        mapWidthRatio = float(mapWidth) / float(maxXY.x)
+        mapHeightRatio = float(mapHeight) / float(maxXY.y)
+
+        # using different ratios for width and height will cause the map to be stretched. So, we have to determine
+        # the global ratio that will perfectly fit into the given image dimension
+        self.globalRatio = min(mapWidthRatio, mapHeightRatio);
+
+        #now we need to readjust the padding to ensure the map is always drawn on the center of the given image dimension
+        self.heightPadding = (height - (self.globalRatio * maxXY.y)) / 2.0
+        self.widthPadding = (width - (self.globalRatio * maxXY.x)) / 2.0
+        self.offsetPoint = minXY;
+        
+        return points
+        
+        
+        
