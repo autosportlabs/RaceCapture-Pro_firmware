48cac90594b5802a1c0bd9766c394450ac5122e0
Brent Picasso <brent@autosportlabs.com>
2014-06-19 07:48:55 -0700

    restored pyinstaller call now that paths / packages are setup correctly.

--- install/buildwininstall.bat
+++ install/buildwininstall.bat
@@ -1,6 +1,6 @@
 @echo off
 del dist /f/s/q
 del build /f/s/q
-pyinstaller.py -y racecapture.spec
+pyinstaller -y racecapture.spec
 "C:\Program Files (x86)\NSIS\makensis.exe" -DVERSION_STRING="0.0.1" raceCaptureApp.nsi
 dist\racecapture\racecapture
