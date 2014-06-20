f73f0cd23bf234a628830d97047e032dee0f1b39
Chris Rae <clr@chrisrae.com>
2014-06-20 10:43:16 +0200

    Win install now takes version # as parameter

--- install/buildwininstall.bat
+++ install/buildwininstall.bat
@@ -1,6 +1,13 @@
 @echo off
+if %1.==. goto nover
 del dist /f/s/q
 del build /f/s/q
 pyinstaller -y racecapture.spec
-"C:\Program Files (x86)\NSIS\makensis.exe" -DVERSION_STRING="0.0.0" raceCaptureApp.nsi
+"C:\Program Files (x86)\NSIS\makensis.exe" -DVERSION_STRING="%1" raceCaptureApp.nsi
 dist\racecapture\racecapture
+goto end
+:nover
+echo Please specify version number as parameter, e.g.:
+echo.   
+echo   %0 1.0.0
+:end
