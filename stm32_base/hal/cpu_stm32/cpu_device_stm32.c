4f6125e77fea1b014532801fdaee5858de5eb017
Brent Picasso <brent@autosportlabs.com>
2014-08-13 21:18:54 -0700

    added to CPU init setting the VTOR

--- stm32_base/hal/cpu_stm32/cpu_device_stm32.c
+++ stm32_base/hal/cpu_stm32/cpu_device_stm32.c
@@ -1,8 +1,11 @@
 #include "cpu_device.h"
 #include <stm32f4xx_misc.h>
+#include <stdint.h>
 
+extern uint32_t _flash_start;
 
 int cpu_device_init(void){
+	NVIC_SetVectorTable(NVIC_VectTab_FLASH, _flash_start);
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 	return 1;
 }
