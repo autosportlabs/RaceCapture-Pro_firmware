bac10c1ac3d949a0799dc608cc5a580f1b7b4131
Brent Picasso <brent@autosportlabs.com>
2014-10-21 22:50:26 -0700

    fixed unit tests to adjust for API changes

--- test/logger_mock/CAN_device_mock.c
+++ test/logger_mock/CAN_device_mock.c
@@ -1,17 +1,17 @@
 #include "CAN_device.h"
 
-int CAN_device_init(size_t channel, uint32_t baud){
+int CAN_device_init(uint8_t channel, uint32_t baud){
 
 }
 
-int CAN_device_tx_msg(CAN_msg *msg, unsigned int timeoutMs){
+int CAN_device_tx_msg(uint8_t channel, CAN_msg *msg, unsigned int timeoutMs){
 	return 1;
 }
 
-int CAN_device_rx_msg(CAN_msg *msg, unsigned int timeoutMs){
+int CAN_device_rx_msg(uint8_t channel, CAN_msg *msg, unsigned int timeoutMs){
 	return 1;
 }
 
-int CAN_device_set_filter(uint8_t id, uint8_t extended, uint32_t filter, uint32_t mask){
+int CAN_device_set_filter(uint8_t channel, uint8_t id, uint8_t extended, uint32_t filter, uint32_t mask){
 	return 1;
 }
