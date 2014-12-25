7eb743c428e7725850e97687c0657e97d4f3bb58
Jean-Sebastien Stoezel <js.stoezel@gmail.com>
2014-12-25 15:49:32 -0600

    Rearranging arguments

--- check_elf_size.sh
+++ check_elf_size.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 3 ]; then
   echo ""
   echo "*** ERROR: invalid number of arguments ($#)"
   echo ""
-  echo "Usage: $0 <size_exec_name> <max_size>"
+  echo "Usage: $0 <elf_filename><size_exec_name> <max_size>"
   exit 1
 fi
 
 max_size=$3
 size_exec_name=$2
-elf_name=$1
-actual_size=$($size_exec_name $elf_name | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
+elf_filename=$1
+actual_size=$($size_exec_name $elf_filename | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')
 
 if [ "$actual_size" -gt "$max_size" ]; then 
   echo "*** ERROR: ELF size too big"
