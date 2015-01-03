e558a11b7ef92711797c576415276380f059f16b
Jean-Sebastien Stoezel <js.stoezel@gmail.com>
2015-01-02 19:33:47 -0600

    Added prototype for flash memory reads

--- include/memory/memory.h
+++ include/memory/memory.h
@@ -2,5 +2,6 @@
 #define MEMORY_H_
 
 int memory_flash_region(const void *vAddress, const void *vData, unsigned int length);
+void memory_read_region(const void *address, void * const data, unsigned int length);
 
 #endif /* MEMORY_H_ */
