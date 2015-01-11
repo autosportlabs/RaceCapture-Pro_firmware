3013ea1d725d16bdbfc66ee23fc8d80b0358a69a
Jean-Sebastien Stoezel <js.stoezel@gmail.com>
2015-01-10 20:17:06 -0600

    Revert "Added prototype for flash memory reads"
    This reverts commit e558a11b7ef92711797c576415276380f059f16b.

--- include/memory/memory.h
+++ include/memory/memory.h
@@ -2,6 +2,5 @@
 #define MEMORY_H_
 
 int memory_flash_region(const void *vAddress, const void *vData, unsigned int length);
-void memory_read_region(const void *address, void * const data, unsigned int length);
 
 #endif /* MEMORY_H_ */
