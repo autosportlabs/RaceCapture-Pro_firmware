9a6cc90e889fd635f417c88c08b0347698d33b9a
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2015-01-29 21:07:48 -0600

    WTF.. how is this possible

--- src/gps/geopoint.c
+++ src/gps/geopoint.c
@@ -7,6 +7,7 @@
 
 #include "geopoint.h"
 #include "gps.h"
+#include <math.h>
 
 #define M_PI 3.14159265358979323846
 
@@ -17,6 +18,7 @@
  * https://github.com/bnoordhuis/bspc/blob/master/deps/qcommon/cm_trace.c
  * http://h14s.p5r.org/2012/09/0x5f3759df.html
  */
+/*
 static float fast_square_root(float number) {
 	long i;
 	float x, y;
@@ -31,10 +33,12 @@ static float fast_square_root(float number) {
 	y  = y * ( f - ( x * y * y ) );
 	return number * y;
 }
+*/
 
 /*
  * source: http://www.ganssle.com/articles/atrig.htm
  */
+/*
 float cosine(float x)
 {
 	float p0,p1,p2,p3,p4,p5,y,t,absx,frac,quad,pi2;
@@ -45,20 +49,21 @@ float cosine(float x)
 	p3=-0.0013888361399;
 	p4= 0.00002476016134;
 	p5=-0.00000026051495;
-	pi2=1.570796326794896; 		/* pi/2 */
+	pi2=1.570796326794896; 		// pi/2
 	absx=x;
-	if (x<0) absx=-absx; 	     /* absolute value of input */
-	quad=(int) (absx/pi2);       	/* quadrant (0 to 3) */
-	frac= (absx/pi2) - quad;     	/* fractional part of input */
+	if (x<0) absx=-absx; 	     // absolute value of input
+	quad=(int) (absx/pi2);       	// quadrant (0 to 3)
+	frac= (absx/pi2) - quad;     	// fractional part of input
 	if(quad==0) t=frac * pi2;
 	if(quad==1) t=(1-frac) * pi2;
 	if(quad==2) t=frac * pi2;
 	if(quad==3) t=(frac-1) * pi2;
 	t=t * t;
 	y=p0 + (p1*t) + (p2*t*t) + (p3*t*t*t) + (p4*t*t*t*t) + (p5*t*t*t*t*t);
-	if((quad==2) | (quad==1)) y=-y;  /* correct sign */
+	if((quad==2) | (quad==1)) y=-y;  // correct sign
 	return(y);
 }
+*/
 
 /**
  * Converts a given value to radians.
@@ -66,17 +71,19 @@ float cosine(float x)
  * @return The radian value.
  */
 static float toRad(float val) {
-	return val * M_PI / 180.0;
+   return val * (M_PI / 180.0);
 }
 
 float distPythag(const GeoPoint *a, const GeoPoint *b) {
-	float dLatRad = toRad(b->latitude - a->latitude);
-	float dLonRad = toRad(b->longitude - a->longitude);
-	float latARad = toRad(a->latitude);
-	float latBRad = toRad(b->latitude);
+	const float dLatRad = toRad(b->latitude - a->latitude);
+	const float dLonRad = toRad(b->longitude - a->longitude);
+	const float latARad = toRad(a->latitude);
+	const float latBRad = toRad(b->latitude);
+	//const float tmp = dLonRad * cosine((latARad + latBRad) / 2);
+   const float tmp = dLonRad * cos((latARad + latBRad) / 2);
 
-	float tmp = dLonRad * cosine((latARad + latBRad) / 2);
-	return fast_square_root(tmp * tmp + dLatRad * dLatRad) * GP_EARTH_RADIUS_M;
+	//return fast_square_root(tmp * tmp + dLatRad * dLatRad) * GP_EARTH_RADIUS_M;
+   return sqrt(tmp * tmp + dLatRad * dLatRad) * GP_EARTH_RADIUS_M;
 }
 
 int isValidPoint(const GeoPoint *p) {
