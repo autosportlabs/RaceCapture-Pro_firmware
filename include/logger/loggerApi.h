1c24a449c3caeef320211597a2f325f31c2fe67f
Brent Picasso <brent@autosportlabs.com>
2015-04-16 00:43:10 -0700

    added CR header

--- include/logger/loggerApi.h
+++ include/logger/loggerApi.h
@@ -1,10 +1,20 @@
-/*
- * loggerApi.h
+/**
+ * AutoSport Labs - Race Capture Pro Firmware
  *
- *  Created on: Jun 10, 2013
- *      Author: brent
+ * Copyright (C) 2014 AutoSport Labs
+ *
+ * This file is part of the Race Capture Pro firmware suite
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with this code. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #ifndef LOGGERAPI_H_
 #define LOGGERAPI_H_
 
