3726cf793d023344220d5ecb99e5d432c50e9a0d
Brent Picasso <brent@autosportlabs.com>
2015-04-15 20:58:34 -0700

    allow BT init status to succeed, even if it failed to provision, to handle cases if BT is already paired and active, being in transparent mode (and not able to respond to AT commands

--- src/devices/bluetooth.c
+++ src/devices/bluetooth.c
@@ -148,20 +148,17 @@ int bt_init_connection(DeviceConfig *config) {
             break;
     }
 
-    int rc;
     if (*rate > 0){
         config->serial->init(8, 0, 1, targetBaud);
         pr_info("BT: Init complete\r\n");
         g_bluetooth_status = BT_STATUS_PROVISIONED;
-        rc = DEVICE_INIT_SUCCESS;
     }
     else{
         pr_info("BT: Failed to provision module. Assuming already connected.\r\n");
         g_bluetooth_status = BT_STATUS_ERROR;
-        rc = DEVICE_INIT_FAIL;
     }
 
-    return rc;
+    return DEVICE_INIT_SUCCESS;
 }
 
 int bt_check_connection_status(DeviceConfig *config) {
