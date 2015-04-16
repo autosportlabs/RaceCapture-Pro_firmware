9bbb29a8f64ea2f7dabf879dea351217cf00c24f
Brent Picasso <brent@autosportlabs.com>
2015-04-15 19:41:18 -0700

    WIP for getting unit tests to work

--- test/mock_gps_device.c
+++ test/mock_gps_device.c
@@ -6,8 +6,8 @@
 #include "gps_device.h"
 #include "serial.h"
 
-int GPS_device_init(Serial *serial) {
-   return 1;
+gps_status_t GPS_device_init(uint8_t targetSampleRate, Serial *serial) {
+   return GPS_STATUS_PROVISIONED;
 }
 
 gps_msg_result_t GPS_device_get_update(GpsSample *gpsSample, Serial *serial) {
