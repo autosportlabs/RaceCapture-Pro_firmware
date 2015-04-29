2186f8efbcc34dafb50a53b3627c143b33c10a2c
Brent Picasso <brent@autosportlabs.com>
2015-04-29 13:36:38 -0700

    removed defunct #define's

--- include/memory/memory.h
+++ include/memory/memory.h
@@ -6,9 +6,6 @@ typedef enum {
     MEMORY_FLASH_WRITE_ERROR = -1
 } memory_flash_result_t;
 
-#define FLASH_SUCCESS		0
-#define FLASH_WRITE_ERROR	-1
-
 memory_flash_result_t memory_flash_region(const void *vAddress, const void *vData, unsigned int length);
 
 #endif /* MEMORY_H_ */
