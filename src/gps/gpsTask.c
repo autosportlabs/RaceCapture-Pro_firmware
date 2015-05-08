8d9304dc6d36f0964b0b74561c5ff0819bf4caa6
Brent Picasso <brent@autosportlabs.com>
2015-05-08 14:00:11 -0700

    more Astyle refomatting

--- src/gps/gpsTask.c
+++ src/gps/gpsTask.c
@@ -19,31 +19,38 @@
 
 static bool g_enableGpsDataLogging = false;
 
-void setGpsDataLogging(bool enable) {
-   g_enableGpsDataLogging = enable;
+void setGpsDataLogging(bool enable)
+{
+    g_enableGpsDataLogging = enable;
 }
 
-void GPSTask(void *pvParameters) {
-	Serial *serial = get_serial(SERIAL_GPS);
-	uint8_t targetSampleRate = decodeSampleRate(getWorkingLoggerConfig()->GPSConfigs.speed.sampleRate);
-	lapStats_init();
-	gps_status_t gps_status = GPS_init(targetSampleRate, serial);
-	if (!gps_status){
-		pr_error("GPS: Error provisioning\r\n");
-	}
+void GPSTask(void *pvParameters)
+{
+    Serial *serial = get_serial(SERIAL_GPS);
+    uint8_t targetSampleRate = decodeSampleRate(getWorkingLoggerConfig()->GPSConfigs.speed.sampleRate);
+    lapStats_init();
+    gps_status_t gps_status = GPS_init(targetSampleRate, serial);
+    if (!gps_status)
+    {
+        pr_error("GPS: Error provisioning\r\n");
+    }
 
-	for (;;) {
-		gps_msg_result_t result = GPS_processUpdate(serial);
-		if (result == GPS_MSG_SUCCESS){
-                   const GpsSnapshot snap = getGpsSnapshot();
-			lapstats_processUpdate(&snap);
-		}
-		else{
-			pr_warning("GPS: timeout\r\n");
-		}
-   }
+    for (;;)
+    {
+        gps_msg_result_t result = GPS_processUpdate(serial);
+        if (result == GPS_MSG_SUCCESS)
+        {
+            const GpsSnapshot snap = getGpsSnapshot();
+            lapstats_processUpdate(&snap);
+        }
+        else
+        {
+            pr_warning("GPS: timeout\r\n");
+        }
+    }
 }
 
-void startGPSTask(int priority){
-	xTaskCreate( GPSTask, ( signed portCHAR * )"GPSTask", GPS_TASK_STACK_SIZE, NULL, priority, NULL );
+void startGPSTask(int priority)
+{
+    xTaskCreate( GPSTask, ( signed portCHAR * )"GPSTask", GPS_TASK_STACK_SIZE, NULL, priority, NULL );
 }
