e4b6c110ff514010f5a596b15e7bfa90bb4a183f
Andrew Stiegmann (stieg) <andrew.stiegmann@flixster-inc.com>
2015-07-02 17:14:29 -0800

    Add more improvemnets to vagrant_setup.sh and add missing packages
    Adds needed packages for Ubuntu image.  Also fixes some issues when
    it comes to setup and re-setup.

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -5,18 +5,6 @@ VAGRANT_SETUP_FILE="$VAGRANT_HOME/.asl_vagrant_setup_complete"
 VAGRANT_S3_FILE_URI="https://s3-us-west-2.amazonaws.com/asl-firmware/vagrant_setup"
 ASL_PROFILE="/etc/profile.d/asl.sh"
 
-# exec this script as a vagrant user if run as root.
-if [ "$EUID" = 0 ]; then
-    echo "Dropping privileges to vagrant user" >&2
-    exec su vagrant -c "$0"
-fi
-
-get_vagrant_file() {
-    for name in $@; do
-	curl "$VAGRANT_S3_FILE_URI/$name"
-    done
-}
-
 set -e
 [ -n "$DEBUG" ] && set -x
 
@@ -25,20 +13,36 @@ if [ -e "$VAGRANT_SETUP_FILE" ]; then
     exit 0
 fi
 
+# exec this script as a vagrant user if run as root.
+if [ "$EUID" = 0 ]; then
+    echo "Dropping privileges to vagrant user" >&2
+    exec su vagrant -c "$0"
+fi
+
+get_vagrant_file() {
+    for name in $@; do
+        echo "Fetching $name" >&2
+	     curl -s "$VAGRANT_S3_FILE_URI/$name"
+    done
+}
+
 # Install the needed packages
+sudo apt-get update
 sudo apt-get -y install \
-	emacs24-nox \
-	clang \
-	gcc \
-	libc++6 \
-	libc++-dev \
-	libcppunit-1.13-0 \
-	libcppunit-dev \
-	python-pip \
-	git \
-	gcc-arm-none-eabi \
-	python-crcmod \
-	minicom \
+	  emacs24-nox \
+	  clang \
+	  gcc \
+	  libc++6 \
+	  libc++-dev \
+	  libcppunit-1.13-0 \
+	  libcppunit-dev \
+	  python-pip \
+	  git \
+	  gcc-arm-none-eabi \
+	  python-crcmod \
+	  minicom \
+	  gzip \
+	  zip \
 
 # Install Toolchains for MK1 and MK2
 unset asl_bins
@@ -52,12 +56,15 @@ done
 
 # Install ihexpy first as asl-f4-loader
 cd
+echo "Installing ihexpy..." >&2
+[ -d ihexpy ] && rm -rf ihexpy
 git clone https://github.com/Jeff-Ciesielski/ihexpy.git ihexpy
 cd ihexpy
 sudo python setup.py install
 
 # Install the ASL bootloader needed for MK2
 cd
+echo "Installing asl_f4_loader..." >&2
 get_vagrant_file 'asl_f4_loader-0.0.6.tgz' | tar xz
 cd asl_f4_loader-0.0.6
 sudo python setup.py install
