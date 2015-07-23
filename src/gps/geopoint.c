df95e10bcb74fdb711fbf293cf8b2b95a995430f
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2015-07-23 15:22:09 -0500

    Add gps_bearing support.  Fix up cpp unit tests to test it
    Add the method that will allow us to calculate the bearing based
    on our current GPS point and the previous one.
    
    Also adds unit tests to ensure that its working.  Note that I had
    to refactor them a bit so that they use a CLOSE ENOUGH style of
    test.

--- src/gps/geopoint.c
+++ src/gps/geopoint.c
@@ -15,12 +15,22 @@
 
 /**
  * Converts a given value to radians.
- * @param val The provided value in non-radian form.
+ * @param val The provided value in degree form (0 - 360).
  * @return The radian value.
  */
 static float toRad(float val)
 {
     return val * (M_PI / 180.0);
+}
+
+/**
+ * Converts a given value to degrees.
+ * @param val The provided value in radian form.
+ * @return The degree value.
+ */
+static float toDeg(float val)
+{
+    return val * (180.0 / M_PI);
 }
 
 float distPythag(const GeoPoint *a, const GeoPoint *b)
@@ -33,6 +43,27 @@ float distPythag(const GeoPoint *a, const GeoPoint *b)
     const float tmp = dLonRad * cos((latARad + latBRad) / 2);
 
     return sqrt(tmp * tmp + dLatRad * dLatRad) * GP_EARTH_RADIUS_M;
+}
+
+float gps_bearing(const GeoPoint *last, const GeoPoint *curr)
+{
+        /*
+         * Algorithm adapted from
+         * http://www.movable-type.co.uk/scripts/latlong.html
+         * Had to use this one as bearing is a relative calculation.
+         * Hopefully this doesn't overflow the float values.
+         */
+
+        const float d_lon = toRad(curr->longitude) - toRad(last->longitude);
+        const float lat_curr = toRad(curr->latitude);
+        const float lat_last = toRad(last->latitude);
+
+        const float y = sin(d_lon) * cos(lat_curr);
+        const float x = cos(lat_last) * sin(lat_curr) -
+                sin(lat_last) * cos(lat_curr) * cos(d_lon);
+
+        float res = toDeg(atan2(y, x));
+        return res < 0 ? res + 360 : res;
 }
 
 int isValidPoint(const GeoPoint *p)
