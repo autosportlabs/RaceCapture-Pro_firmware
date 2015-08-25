7dd7cb57f5aa402867ed5186c8882b85abc0c236
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2015-08-24 21:06:10 -0500

    Cleanup the LED code
    Initially I thought that the multiple calls to the initiaize GPIO code
    were causing previous initilizations to become undone.  After reading
    the code I have since learned that this is not the case.
    
    But in the process of cleanup, I did find a minor bug in how we
    maintain state, so I fixed that and now the toggle functions will work
    as expected.

--- RCT/hal/LED_stm32/LED_device_stm32.c
+++ RCT/hal/LED_stm32/LED_device_stm32.c
@@ -1,19 +1,48 @@
+/*
+ * Race Capture Pro Firmware
+ *
+ * Copyright (C) 2015 Autosport Labs
+ *
+ * This file is part of the Race Capture Pro fimrware suite
+ *
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should
+ * have received a copy of the GNU General Public License along with
+ * this code. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * LED outputs are on GPIOA group.  Pins 0 - 2
+ * 0 - Blue
+ * 1 - Green
+ * 2 - Red
+ */
+
 #include "LED_device.h"
+
 #include <stm32f30x_gpio.h>
 #include <stm32f30x_rcc.h>
 
 struct led {
-    GPIO_TypeDef *port;
     uint16_t mask;
     uint8_t level;
 };
 
-#define LED_COUNT 3
 static struct led leds[] = {
-    {GPIOA, GPIO_Pin_0, 0},
-    {GPIOA, GPIO_Pin_1, 0},
-    {GPIOA, GPIO_Pin_2, 0}
+        {GPIO_Pin_0, 0},
+        {GPIO_Pin_1, 0},
+        {GPIO_Pin_2, 0}
 };
+
+#define LED_COUNT (sizeof(leds) / sizeof(struct led))
 
 int LED_device_init(void)
 {
@@ -30,11 +59,14 @@ int LED_device_init(void)
     gpio_conf.GPIO_Mode = GPIO_Mode_OUT;
     gpio_conf.GPIO_OType = GPIO_OType_PP;
 
-    for (i = 0; i < LED_COUNT; ++i) {
-        gpio_conf.GPIO_Pin = leds[i].mask;
-        GPIO_Init(leds[i].port, &gpio_conf);
-        LED_device_disable(i);
-    }
+    for (i = 0; i < LED_COUNT; ++i)
+        gpio_conf.GPIO_Pin |= leds[i].mask;
+
+    GPIO_Init(GPIOA, &gpio_conf);
+
+    for (i = 0; i < LED_COUNT; ++i)
+            LED_device_disable(i);
+
     return 1;
 }
 
@@ -42,14 +74,18 @@ void LED_device_enable(unsigned int led)
 {
     if (led >= LED_COUNT)
         return;
-    GPIO_SetBits(leds[led].port, leds[led].mask);
+
+    leds[led].level = 1;
+    GPIO_SetBits(GPIOA, leds[led].mask);
 }
 
 void LED_device_disable(unsigned int led)
 {
     if (led >= LED_COUNT)
         return;
-    GPIO_ResetBits(leds[led].port, leds[led].mask);
+
+    leds[led].level = 0;
+    GPIO_ResetBits(GPIOA, leds[led].mask);
 }
 
 void LED_device_toggle(unsigned int led)
@@ -57,11 +93,5 @@ void LED_device_toggle(unsigned int led)
     if (led >= LED_COUNT)
         return;
 
-    if (leds[led].level == 0) {
-        leds[led].level = 1;
-        GPIO_ResetBits(leds[led].port, leds[led].mask);
-    } else {
-        leds[led].level = 0;
-        GPIO_SetBits(leds[led].port, leds[led].mask);
-    }
+    leds[led].level ? LED_device_disable(led) : LED_device_enable(led);
 }
