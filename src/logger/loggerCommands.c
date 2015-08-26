00fc44e2eb4167793248919bd22162eb828600aa
Brent Picasso <brent@autosportlabs.com>
2015-08-26 10:21:30 -0700

    removed one more #if

--- src/logger/loggerCommands.c
+++ src/logger/loggerCommands.c
@@ -24,9 +24,7 @@
 #include "taskUtil.h"
 #include "cpu.h"
 #include "taskUtil.h"
-#if LUA_SUPPORT == 1
 #include "luaScript.h"
-#endif
 
 void TestSD(Serial *serial, unsigned int argc, char **argv)
 {
