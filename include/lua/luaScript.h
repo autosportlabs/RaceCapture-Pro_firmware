a3ad83c14fb0771ddad4e201cdf9f428fa7409be
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2015-10-02 16:38:43 -0500

    Revert "Merge pull request #92 from autosportlabs/p/stieg/fix_mk1_prog_crash"
    This reverts commit c24c96fb5fb920322b5603170723075ca17e55eb, reversing
    changes made to 4df22f0ee3227d975b6e4659d7ac1fb63c402074.
    
    Revert broken merge :(.  Broke unit tests.

--- include/lua/luaScript.h
+++ include/lua/luaScript.h
@@ -1,49 +1,24 @@
-/*
- * Race Capture Pro Firmware
- *
- * Copyright (C) 2015 Autosport Labs
- *
- * This file is part of the Race Capture Pro fimrware suite
- *
- * This is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * See the GNU General Public License for more details. You should
- * have received a copy of the GNU General Public License along with
- * this code. If not, see <http://www.gnu.org/licenses/>.
- */
-
 #ifndef LUASCRIPT_H_
 #define LUASCRIPT_H_
 
-#include "capabilities.h"
-#include "memory.h"
-
 #include <stdint.h>
+#include "memory.h"
+#include "capabilities.h"
 
-enum script_add_result {
-        SCRIPT_ADD_RESULT_FAIL = 0,
-        SCRIPT_ADD_RESULT_OK = 1,
-};
+#define SCRIPT_ADD_RESULT_OK  		1
+#define SCRIPT_ADD_RESULT_FAIL  	0
 
-enum script_add_mode {
-        SCRIPT_ADD_MODE_IN_PROGRESS = 1,
-        SCRIPT_ADD_MODE_COMPLETE = 2,
-};
+#define SCRIPT_ADD_MODE_IN_PROGRESS	1
+#define SCRIPT_ADD_MODE_COMPLETE 	2
 
-#define MAGIC_NUMBER_SCRIPT_INIT	0xBADDECAF
-#define SCRIPT_PAGE_SIZE	256
-#define MAX_SCRIPT_PAGES	(SCRIPT_MEMORY_LENGTH / SCRIPT_PAGE_SIZE)
+#define MAGIC_NUMBER_SCRIPT_INIT 0xBADDECAF
+
+#define SCRIPT_PAGE_SIZE 256
+#define MAX_SCRIPT_PAGES SCRIPT_MEMORY_LENGTH / SCRIPT_PAGE_SIZE
 
 typedef struct _ScriptConfig {
-        char script[SCRIPT_MEMORY_LENGTH - 4];
-        uint32_t magicInit;
+    char script[SCRIPT_MEMORY_LENGTH - 4];
+    uint32_t magicInit;
 } ScriptConfig;
 
 void initialize_script();
@@ -52,8 +27,7 @@ int flash_default_script();
 
 const char * getScript();
 
-enum script_add_result flashScriptPage(unsigned int page, const char *data,
-                                       enum script_add_mode mode);
+int flashScriptPage(unsigned int page, const char *data, int mode);
 
 void unescapeScript(char *data);
 
