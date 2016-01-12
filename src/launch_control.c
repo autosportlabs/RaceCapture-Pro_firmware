1b56557e2f9a79aa79d615557d1f05724ebbc23b
Andrew Stiegmann <andrew.stiegmann@gmail.com>
2016-01-11 21:45:57 -0600

    Merge pull request #363 from autosportlabs/p/stieg/lap_stats_fixes
    P/stieg/lap stats fixes
    
    Backport of lap_stats_fixes from 2.9.0 to 2.8.7
    Issues: #6

--- src/launch_control.c
+++ src/launch_control.c
@@ -1,21 +1,22 @@
-/**
- * Race Capture Firmware
+/*
+ * Race Capture Pro Firmware
  *
- * Copyright (C) 2014 Autosport Labs
+ * Copyright (C) 2016 Autosport Labs
  *
- * This file is part of the Race Capture firmware suite
+ * This file is part of the Race Capture Pro fimrware suite
  *
- * This is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
- * See the GNU General Public License for more details. You should have received a copy of the GNU
- * General Public License along with this code. If not, see <http://www.gnu.org/licenses/>.
- *
- * Authors: Stieg
+ * See the GNU General Public License for more details. You should
+ * have received a copy of the GNU General Public License along with
+ * this code. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "launch_control.h"
@@ -74,18 +75,13 @@ tiny_millis_t lc_getLaunchTime()
 void lc_reset()
 {
     g_startTime = -1;
-    g_geoCircle = (struct GeoCircle) {
-        {
-            0
-        }
-    };  // GCC Bug 53119.
     g_hasLaunched = false;
 }
 
 void lc_setup(const Track *track, const float targetRadius)
 {
-    lc_reset();
     g_geoCircle = gc_createGeoCircle(getStartPoint(track), targetRadius);
+    lc_reset();
 }
 
 void lc_supplyGpsSnapshot(const GpsSnapshot *snap)
---
+++ test/lap_stats/LapStatsTest.hh
@@ -0,0 +1,74 @@
+/*
+ * Race Capture Pro Firmware
+ *
+ * Copyright (C) 2016 Autosport Labs
+ *
+ * This file is part of the Race Capture Pro fimrware suite
+ *
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should
+ * have received a copy of the GNU General Public License along with
+ * this code. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LAPSTATSTEST_H_
+#define _LAPSTATSTEST_H_
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class LapStatsTest : public CppUnit::TestFixture
+{
+        CPPUNIT_TEST_SUITE( LapStatsTest );
+        CPPUNIT_TEST( test_init );
+        CPPUNIT_TEST( null_track_test );
+        CPPUNIT_TEST( invalid_track_test );
+        CPPUNIT_TEST( automatic_track_test );
+        CPPUNIT_TEST( manual_track_test );
+        CPPUNIT_TEST( sectors_disabled_test );
+        CPPUNIT_TEST( sectors_enabled_test );
+        CPPUNIT_TEST( circuit_lap_start_event_test );
+        CPPUNIT_TEST( stage_lap_start_event_test );
+        CPPUNIT_TEST( circuit_lap_finish_event_test );
+        CPPUNIT_TEST( stage_lap_finish_event_test );
+        CPPUNIT_TEST( sector_boundary_event_test );
+        CPPUNIT_TEST( update_distance_test );
+        CPPUNIT_TEST( update_distance_no_prev_point_test );
+        CPPUNIT_TEST( update_sector_geo_circle_test );
+        CPPUNIT_TEST( update_elapsed_time_test );
+        CPPUNIT_TEST( at_sf_reset_test );
+        CPPUNIT_TEST( at_sector_reset_test );
+        CPPUNIT_TEST_SUITE_END();
+
+public:
+        void setUp();
+        void tearDown();
+        void test_init();
+        void null_track_test();
+        void invalid_track_test();
+        void automatic_track_test();
+        void manual_track_test();
+        void sectors_disabled_test();
+        void sectors_enabled_test();
+        void circuit_lap_start_event_test();
+        void stage_lap_start_event_test();
+        void circuit_lap_finish_event_test();
+        void stage_lap_finish_event_test();
+        void sector_boundary_event_test();
+        void update_distance_test();
+        void update_distance_no_prev_point_test();
+        void update_sector_geo_circle_test();
+        void update_elapsed_time_test();
+        void at_sf_reset_test();
+        void at_sector_reset_test();
+};
+
+
+#endif /* _LAPSTATSTEST_H_ */
