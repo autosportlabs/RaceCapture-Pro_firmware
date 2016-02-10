ac1c11c52eee2b674b6e00cda080516992114946
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2016-02-09 18:45:52 -0600

    Don't redefine '__packed' macro if already defined

--- stm32_base/hal/usb_stm32/usb_conf.h
+++ stm32_base/hal/usb_stm32/usb_conf.h
@@ -254,7 +254,7 @@
 #define __packed    __packed
 #elif defined (__ICCARM__)     /* IAR Compiler */
 #define __packed    __packed
-#elif defined   ( __GNUC__ )   /* GNU Compiler */
+#elif defined   ( __GNUC__ )   /* GNU Compiler */ && !defined(__packed)
 #define __packed    __attribute__ ((__packed__))
 #elif defined   (__TASKING__)  /* TASKING Compiler */
 #define __packed    __unaligned
@@ -306,4 +306,3 @@
   * @}
   */
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
-
