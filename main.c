d81bfbf0593fb71bb16436a17f171d92da56af14
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2016-04-19 11:16:58 -0700

    Disable WiFi code so we can merge bits back to r/2.10.0

--- main.c
+++ main.c
@@ -83,7 +83,7 @@ void setupTask(void *delTask)
         startOBD2Task(RCP_INPUT_PRIORITY);
         startConnectivityTask(RCP_OUTPUT_PRIORITY);
         startLoggerTaskEx(RCP_LOGGING_PRIORITY);
-        wifi_init_task(RCP_OUTPUT_PRIORITY, RCP_INPUT_PRIORITY);
+        /* wifi_init_task(RCP_OUTPUT_PRIORITY, RCP_INPUT_PRIORITY); */
 
 #if defined(USB_SERIAL_SUPPORT)
         startUSBCommTask(RCP_INPUT_PRIORITY);
