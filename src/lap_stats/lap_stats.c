b0bae5c25a2c5cd8ee068d2c66bbbe83cdea406a
Andrew Stiegmann (stieg) <andrew@autosportlabs.com>
2016-06-21 09:53:01 -0500

    Apply Brent's Feedback

--- src/lap_stats/lap_stats.c
+++ src/lap_stats/lap_stats.c
@@ -19,7 +19,6 @@
  * this code. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 #include "auto_track.h"
 #include "dateTime.h"
 #include "geoCircle.h"
@@ -36,17 +35,14 @@
 #include "predictive_timer_2.h"
 #include "printk.h"
 #include "tracks.h"
-
 #include <stdint.h>
 
 /* Make the radius 2x the size of start/finish radius.*/
 #define GEO_TRIGGER_RADIUS_MULTIPLIER 2
-#define KMS_TO_MILES_CONSTANT (.621371)
-
-// In Millis now.
-#define START_FINISH_TIME_THRESHOLD 10000
-
-#define TIME_NULL -1
+#define KMS_TO_MILES_CONSTANT         (0.621371)
+#define MEASUREMENT_SPEED_MIN_KPH     1
+#define START_FINISH_TIME_THRESHOLD   10000
+#define TIME_NULL                     -1
 
 static const Track *g_active_track;
 static float g_geo_circle_radius;
@@ -134,17 +130,19 @@ static void update_distance(const GpsSnapshot *gps_ss)
         const float speed_avg =
                 (gps_ss->sample.speed + gps_ss->previous_speed) / 2;
 
-        if (speed_avg < 1)
+        /*
+         * Filter out low speed measurements to prevent updates when
+         * a vehicle is stationary or moving at trivial speed.
+         */
+        if (speed_avg < MEASUREMENT_SPEED_MIN_KPH)
                 return;
 
         /*
          * Speed: KM/H
          * Delta ms: ms
-         * KM/H * delta ms / 3600 = distance.  To save a divide
-         * we do KM/H * delta ms * (1 / 3600)
+         * KM/H * delta ms / 3600 = delta distance.
          */
-        static const float factor = 1 / 3600;
-        g_distance += speed_avg * gps_ss->delta_last_sample * factor;
+        g_distance += speed_avg * gps_ss->delta_last_sample / 3600;
 }
 
 static void set_distance(const float distance)
