9f328bd587995be10d6df2554dd609287006a369
Brent Picasso <brent@autosportlabs.com>
2017-04-23 13:44:45 -0700

    make GPS hardware reset and init more robust #919

--- src/devices/gps_skytraq_s1216_sup500f8.c
+++ src/devices/gps_skytraq_s1216_sup500f8.c
@@ -1,3 +1,23 @@
+/*
+ * Race Capture Pro Firmware
+ *
+ * Copyright (C) 2017 Autosport Labs
+ *
+ * This file is part of the Race Capture Pro fimrware suite
+ *
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should
+ * have received a copy of the GNU General Public License along with
+ * this code. If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "FreeRTOS.h"
 #include "at_basic.h"
 #include "byteswap.h"
@@ -725,7 +745,6 @@ gps_status_t GPS_device_init(uint8_t sampleRate, struct Serial *serial)
             } else {
                 pr_error("GPS: Error: "
                          "could not detect GPS module on known baud rates\r\n");
-                gps_init_status = GPS_STATUS_ERROR;
                 break;
             }
         }
