90ee1f239ab122eecb85659d794be548c1bc6767
Brent Picasso <brent@autosportlabs.com>
2017-07-03 14:55:26 -0700

    call 64 bit swap (fix previous commit)

--- src/util/byteswap.c
+++ src/util/byteswap.c
@@ -65,7 +65,7 @@ uint64_t swap_uint_length(uint64_t val, size_t bit_length)
     if (bit_length <= 16) return swap_uint16(val);
     if (bit_length <= 24) return swap_uint24(val);
     if (bit_length <= 32) return swap_uint32(val);
-    if (bit_length <= 64) return swap_uint32(val);
+    if (bit_length <= 64) return swap_uint64(val);
     panic(PANIC_CAUSE_UNREACHABLE);
     return 0; /* to make compiler happy */
 }
