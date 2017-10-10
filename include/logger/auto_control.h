2297bc62c67da464518ee3f680ba5f9748e30276
Brent Picasso <brent@autosportlabs.com>
2017-10-09 19:20:25 -0700

    added documentation

--- include/logger/auto_control.h
+++ include/logger/auto_control.h
@@ -29,6 +29,10 @@
 
 CPP_GUARD_BEGIN
 
+/**
+ * Provides the generic logic for automatic control, triggered
+ * based on a threshold value and time
+ */
 #define DEFAULT_AUTO_CONTROL_START_THRESHOLD 40
 #define DEFAULT_AUTO_CONTROL_START_TIME_SEC 5
 #define DEFAULT_AUTO_CONTROL_STOP_THRESHOLD 25
@@ -52,29 +56,75 @@ struct auto_control_state {
         tiny_millis_t timestamp_stop;
 };
 
+/**
+ * Resets the trigger with default values.
+ * @param start the start trigger
+ * @return stop the stop trigger
+ */
 void auto_control_reset_trigger(struct auto_control_trigger *start, struct auto_control_trigger *stop);
 
+/**
+ * Sets the initial auto control state
+ * @param the state to initialize
+ */
 void auto_control_init_state(struct auto_control_state *state);
 
+/**
+ * Serialize auto control trigger value to JSON
+ * @param serial the Serial connection where the JSON will be output
+ * @param alst the trigger to output
+ * @param name the name of the trigger type
+ * @param more there are more JSON segments
+ */
 void get_auto_control_trigger(struct Serial* serial,
                            struct auto_control_trigger *alst,
                            const char* name,
                            const bool more);
 
+/**
+ * De-serialize the auto control trigger value from JSON
+ * @param alst the trigger to populate
+ * @param name the name of the trigger parent (e.g. start or stop)
+ * @param root the parent of the JSON object graph
+ */
 void set_auto_control_trigger(struct auto_control_trigger *alst,
                            const char* name,
                            const jsmntok_t* root);
 
+/**
+ * Tests if the trigger should start
+ * @param value the current value to test
+ * @param uptime the current uptime
+ * @param auto_trigger the trigger configuration
+ * @param auto_state the auto-control state
+ * @return true if the control should trigger
+ */
 bool auto_control_should_start(const float current_value,
                                  const tiny_millis_t uptime,
                                  const struct auto_control_trigger * auto_trigger,
                                  struct auto_control_state * auto_state);
 
+/**
+ * Tests if the trigger should stop
+ * @param value the current value to test
+ * @param uptime the current uptime
+ * @param auto_trigger the trigger configuration
+ * @param auto_state the auto-control state
+ * @return true if the control should de-trigger
+ */
 bool auto_control_should_stop(const float current_value,
                                 const tiny_millis_t uptime,
                                 const struct auto_control_trigger * auto_trigger,
                                 struct auto_control_state * auto_state);
 
+
+/**
+ * @param value the value to check
+ * @param start the start trigger
+ * @param stop the stop trigger
+ * @param the auto-control state
+ * @param an enum indicating what action should be taken
+ */
 enum auto_control_trigger_result auto_control_check_trigger(float value,
                                                             struct auto_control_trigger *start,
                                                             struct auto_control_trigger *stop,
