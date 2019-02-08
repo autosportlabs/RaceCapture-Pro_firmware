ddf5aba559a3c2fb5799c240bad19c2cf22e2188
Brent Picasso <brent@autosportlabs.com>
2019-02-08 09:34:14 -0800

    update documentation

--- include/drivers/shiftx_drv.h
+++ include/drivers/shiftx_drv.h
@@ -34,22 +34,112 @@ struct shiftx_configuration {
         uint8_t auto_brightness_scaling;
 };
 
+/**
+ * Process an incoming CAN message
+ * @param msg the CAN message to process
+ */
 void shiftx_handle_can_rx_msg(CAN_msg *msg);
 
+/**
+ * Retreive a pointer to the current runtime configuration
+ * @return pointer to struct of the shiftx_configuration
+ */
 struct shiftx_configuration * shiftx_get_config(void);
+
+
+/**
+ * Send the configuration update message to the connected ShiftX device, using the current runtime configurationi
+ */
 bool shiftx_update_config(void);
 
+/**
+ * Set a discrete LED on the ShiftX device
+ * @param led_index the index of the led to set
+ * @param leds_to_set number of LEDs to set. 0 = set all remaining LEDs
+ * @param red red color value
+ * @param green green color value
+ * @param blue blue color value
+ * @param flash flash rate, in Hz
+ * @return true if CAN message was successfully broadcasted
+ */
 bool shiftx_set_discrete_led(uint8_t led_index, uint8_t leds_to_set, uint8_t red, uint8_t green, uint8_t blue, uint8_t flash);
+
+/**
+ * Set the display value
+ * @param digit_index index of digit to set
+ * @param ascii ascii value to set (48 = '0', 49 = '1')
+ * @return true if CAN message was successfully broadcasted
+ */
 bool shiftx_set_display(uint8_t digit_index, uint8_t ascii);
 
+/**
+ * Configures the linear graph on the ShiftX device
+ * @param rendering_style 0=left->right; 1=center; 2=right->left
+ * @param linear_style 0=smooth/interpolated; 1=stepped
+ * @param low_range low value range of linear display
+ * @param high_range high value range of linear display (ignored if rendering_style = stepped)
+ * @return true if CAN message was successfully broadcasted
+ */
 bool shiftx_config_linear_graph(uint8_t rendering_style, uint8_t linear_style, uint16_t low_range, uint16_t high_range);
+
+/**
+ * Configures a threshold for the linear graph on the ShiftX device
+ * @param threshold_id id of threshold (0-4)
+ * @param segment_length number of segments to display for this threshold (ignored if linear style = smooth)
+ * @param threshold value for this threshold
+ * @param red red color value
+ * @param green green color value
+ * @param blue blue color value
+ * @param flash flash rate, in Hz
+ * @return true if CAN message was successfully broadcasted
+ */
 bool shiftx_set_linear_threshold(uint8_t threshold_id, uint8_t segment_length, uint16_t threshold, uint8_t red, uint8_t green, uint8_t blue, uint8_t flash);
+
+/**
+ * Updates the current value for the linear graph
+ * @param value the current runtime value
+ * @return true if CAN message was successfully broadcasted
+ */
 bool shiftx_update_linear_graph(uint16_t value);
 
+/**
+ * Configures a threshold for an alert on the ShiftX device
+ * @param alert_id id of alert (0-> number of alert indicators on device)
+ * @param threshold_id id of threshold (0-4)
+ * @param threshold value for this threshold
+ * @param red red color value
+ * @param green green color value
+ * @param blue blue color value
+ * @param flash flash rate, in Hz
+ * @return true if CAN message was successfully broadcasted
+ */
 bool shiftx_set_alert_threshold(uint8_t alert_id, uint8_t threshold_id, uint16_t threshold, uint8_t red, uint8_t green, uint8_t blue, uint8_t flash);
+
+/**
+ * Directly set an alert indicator
+ * @param alert_id id of alert (0-> number of alert indicators on device)
+ * @param red red color value
+ * @param green green color value
+ * @param blue blue color value
+ * @param flash flash rate, in Hz
+ * @return true if CAN message was successfully broadcasted
+ */
 bool shiftx_set_alert(uint8_t alert_id, uint8_t red, uint8_t green, uint8_t blue, uint8_t flash);
+
+/**
+ * Updates the current value for the specified alert
+ * @param alert_id id of alert (0-> number of alert indicators on device)
+ * @param value the current runtime value
+ * @return true if CAN message was successfully broadcasted
+ */
 bool shiftx_update_alert(uint8_t alert_id, uint16_t value);
 
+/**
+ * Check if a button has been pressed
+ * @param button_id pointer to the ID of the button event (stored in passed-in parameter if button press was receive)
+ * @param state pointer to state of the button event (1=down, 0 = up) (stored in passed-in parameter if button press was received)
+ * @return true if a button press was received
+ */
 bool shiftx_rx_button_press(uint8_t * button_id, uint8_t * state);
 
 CPP_GUARD_END
